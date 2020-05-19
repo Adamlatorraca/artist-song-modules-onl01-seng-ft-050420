8888b7ebe7b0c883970585518adcc687e9925996
Adam LaTorraca <adam.latorraca@gmail.com>
2020-05-19 22:12:10 +0000

    Automatically backed up by Learn

--- lib/concerns/paramble.rb
+++ lib/concerns/paramble.rb
@@ -1,5 +1,5 @@
 module Paramable
   def to_param
-      Artist.all.name.downcase.gsub(' ', '-') || Song.name.downcase.gsub(' ', '-')
+      Artist.all.name.downcase.gsub(' ', '-') || Song.all.name.downcase.gsub(' ', '-')
   end
 end
